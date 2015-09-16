e1acc401178b97c05f57c8551cee9638a0e18dd6
sihu <sh@clickwerk.ch>
2015-09-16 20:56:59 +0200

    Release 2.0
    *ChangeDate/Sec display
    *Change: Batterybar
    *Fix: Error on hour-change (array-out-of-bound with -1)

--- src/main.c
+++ src/main.c
@@ -1,33 +1,28 @@
 #include <pebble.h>
   
 static Window *s_main_window;
-static TextLayer *s_time_layer, *s_battery_layer, *s_datetext_layer, *s_minutetext_layer, *s_hourtext_layer, *s_connection_layer;
+static TextLayer *s_time_layer, *s_datetext_layer, *s_minutetext_layer, *s_hourtext_layer, *s_connection_layer;
+static Layer *s_canvas_layer;
 
 // performance
 static int min = 0;
 static int hour = 0;
+static int battery = 100;
 
 // dialect
-const char *hour_string[25] = { "zwölfi", "eis","zwei", "drei", "viäri", "füüfi", "säxi",
+const char *hour_string[25] = { "zwölfi", "eis","zwai", "drüü", "viari", "füüfi", "säxi",
 		 "sibni", "achti", "nüni", "zehni", "ölfi"};
 
 const char *weekday_string[7] = { "So", "Mo", "Di","Mi", "Do", "Fr", "Sa" };
 
-const char *min_string[30] = { "\neis ", "\nzwei ", "\ndrii ", "\nviar ", "\nfüüf ", "\nsäx ", "\nsiba ", "\nacht ", "\nnüün ", "\nzäh ", "\nelf ", "\nzwölf ", 
+const char *min_string[30] = { "\neis ", "\nzwai ", "\ndrüü ", "\nviar ", "\nfüüf ", "\nsäx ", "\nsiba ", "\nacht ", "\nnüün ", "\nzäh ", "\nelf ", "\nzwölf ", 
     "\ndrizehn ", "\nviarzehn ", "\nviartel ", "\nsechzehn", "\nsibzehn ", "\nachzehn ", "\nnünzehn ", "\nzwänzg ", 
-    "ainazwenzg\n", "zwaiazwenzg\n", "driazwenzg\n", "viarazwenzg\n", "füfazwenzg\n", "sexazwenzg\n", 
-    "sibanazwenzg\n", "achtazwenzg", "nünazwenzg\n", "\nhalb" };
+    "\n21 ", "22\n", "driazwenzg\n", "viarazwenzg\n", "füfazwenzg\n", "sexazwenzg\n", 
+    "sibanazwenzg\n", "achtazwenzg\n", "nünazwenzg\n", "\nhalb" };
 
 
 static void handle_battery(BatteryChargeState charge_state) {
-  static char battery_text[] = "100%";
-
-  if (charge_state.is_charging) {
-    snprintf(battery_text, sizeof(battery_text), "+");
-  } else {
-    snprintf(battery_text, sizeof(battery_text), "%d%%", charge_state.charge_percent);
-  }
-  text_layer_set_text(s_battery_layer, battery_text);
+  battery = charge_state.charge_percent;
 }
 
 static void handle_minute_tick(struct tm* tick_time, TimeUnits units_changed) {
@@ -40,15 +35,19 @@ static void handle_minute_tick(struct tm* tick_time, TimeUnits units_changed) {
   //time_t currentmktime(struct tm * timeptr)
   min = tick_time->tm_min;
   hour = tick_time->tm_hour > 11 ? tick_time->tm_hour-12 : tick_time->tm_hour;  
-  
-  if(min > 30) {
-    strcpy(minute_text , min_string[60-min-1]);
-    strcat(minute_text , "vor");
-  } else if (min < 30){
-    strcpy(minute_text , min_string[min-1]);
-    strcat(minute_text , "ab");
+
+  if(min > 0){
+    if(min > 30) {
+      strcpy(minute_text , min_string[60-min-1]);
+      strcat(minute_text , "vor");
+    } else if (min < 30){
+      strcpy(minute_text , min_string[min-1]);
+      strcat(minute_text , "ab");
+    } else {
+      strcpy(minute_text , min_string[min-1]);
+    }
   } else {
-    strcpy(minute_text , min_string[min-1]);
+    strcat(minute_text , "");
   }
   strcpy(hour_text , hour_string[min >= 30 ? hour+1 : hour]);
   
@@ -64,14 +63,21 @@ static void handle_minute_tick(struct tm* tick_time, TimeUnits units_changed) {
 
 }
 
+static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds(this_layer);
+
+  // Draw the 'stalk'
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_rect(ctx, GRect(bounds.size.w/4*3, 0, bounds.size.w/4*battery/100, 5), 0, GCornerNone);
+}
 
 static void handle_second_tick(struct tm* tick_time, TimeUnits units_changed) {
   // Needs to be static because it's used by the system later.
-  static char s_time_text[] = "00:00:00";
+  static char s_time_text[] = "00";
   //time_t currentmktime(struct tm * timeptr)
   //snprintf(s_min_text, sizeof(battery_text), "%d%%", charge_state.charge_percent);
   
-  strftime(s_time_text, sizeof(s_time_text), "%T", tick_time);
+  strftime(s_time_text, sizeof(s_time_text), "%S", tick_time);
   text_layer_set_text(s_time_layer, s_time_text);
   
   handle_battery(battery_state_service_peek());
@@ -112,28 +118,23 @@ static void main_window_load(Window *window) {
   text_layer_set_font(s_hourtext_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   
   // Date ()
-  s_datetext_layer = text_layer_create(GRect(0, 110, bounds.size.w, 34));
+  s_datetext_layer = text_layer_create(GRect(40, 110, bounds.size.w, 34));
   text_layer_set_text_color(s_datetext_layer, GColorWhite);
   text_layer_set_background_color(s_datetext_layer, GColorClear);
   text_layer_set_font(s_datetext_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
-  text_layer_set_text_alignment(s_datetext_layer, GTextAlignmentRight);
 
   
   // Time digital
-  s_time_layer = text_layer_create(GRect(0, 140, bounds.size.w, 34));
+  s_time_layer = text_layer_create(GRect(5, 110, bounds.size.w, 34));
   text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentRight);
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
+    
+  // Battery bar
+  s_canvas_layer = layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
+  layer_add_child(window_layer, s_canvas_layer);
+  layer_set_update_proc(s_canvas_layer, canvas_update_proc);
   
-  // Battery-Status (top, right)
-  s_battery_layer = text_layer_create(GRect(0, 0, bounds.size.w, 34));
-  text_layer_set_text_color(s_battery_layer, GColorWhite);
-  text_layer_set_background_color(s_battery_layer, GColorClear);
-  text_layer_set_font(s_battery_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-  text_layer_set_text_alignment(s_battery_layer, GTextAlignmentRight);
-  text_layer_set_text(s_battery_layer, "100%");
-
   // Ensures time is displayed immediately (will break if NULL tick event accessed).
   // (This is why it's a good idea to have a separate routine to do the update itself.)
   time_t now = time(NULL);
@@ -144,16 +145,14 @@ static void main_window_load(Window *window) {
 
   tick_timer_service_subscribe(SECOND_UNIT, handle_second_tick);
 
-
+ 
   battery_state_service_subscribe(handle_battery);
-  //bluetooth_connection_service_subscribe(handle_bluetooth);
 
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_connection_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_minutetext_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_hourtext_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_datetext_layer));
-  layer_add_child(window_layer, text_layer_get_layer(s_battery_layer));
 }
 
 static void main_window_unload(Window *window) {
@@ -162,7 +161,10 @@ static void main_window_unload(Window *window) {
   bluetooth_connection_service_unsubscribe();
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_connection_layer);
-  text_layer_destroy(s_battery_layer);
+  text_layer_destroy(s_minutetext_layer);
+  text_layer_destroy(s_hourtext_layer);
+  text_layer_destroy(s_datetext_layer);
+  layer_destroy(s_canvas_layer);
 }
 
 static void init() {
